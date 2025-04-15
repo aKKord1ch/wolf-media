75c2d054a8d5c500799a467126b531e0a83d804c
Stas Shuvykin <stasiksuvykin@gmail.com>
2025-04-15 11:18:52 +0300

    fix: updated adaptive for 'how-do-we-work' section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head></head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div className="wrapper">
