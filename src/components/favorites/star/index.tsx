395a6c474ac5c21f88d6c34784c05e0c83d6caa7
Stas Shuvykin <stasiksuvykin@gmail.com>
2025-04-27 15:21:52 +0300

    fix: updated svg displaying, updated loading styles for detail-img

--- src/components/favorites/star/index.tsx
+++ src/components/favorites/star/index.tsx
@@ -9,7 +9,6 @@ import {
   removeFromFavorites,
 } from "../../../../store/slices/listReducer";
 import { ListItem } from "../../../../store/slices/interface/listInterface";
-import { getLocalStorageData } from "@/helpers/localStorage";
 import clsx from "clsx";
 
 interface StarProps {
@@ -29,14 +28,36 @@ export default function Star({ data }: StarProps) {
   };
 
   return (
-    <div
-      className={clsx(css.heart, {[css.disabled]: !isFavorite})}
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        action(data);
-      }}
-    >
-    </div>
+      <div
+        className={clsx(css.star)}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          action(data);
+        }}
+      >
+        {isFavorite ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="#FFD700"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 .587l3.668 7.568L24 9.75l-6 5.85 1.416 8.4L12 19.897 4.584 24 6 15.6 0 9.75l8.332-1.595z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          stroke="#FFD700"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 .587l3.668 7.568L24 9.75l-6 5.85 1.416 8.4L12 19.897 4.584 24 6 15.6 0 9.75l8.332-1.595z" />
+        </svg>)}
+      </div>
   );
 }
